0c52e1764b1e106e0896136eb1580e4013f7bfe3
huetruong <truongvshieu01@gmail.com>
2024-07-15 22:08:31 +0700

    update

--- models/User.js
+++ models/User.js
@@ -1,18 +1,22 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['guest', 'student', 'tutor', 'moderator', 'admin'], default: 'guest' },
+  role: {
+    type: String,
+    enum: ["guest", "student", "tutor", "moderator", "admin"],
+    default: "guest",
+  },
   degree: String,
   skills: [String],
-  identityInfo: String
+  identityInfo: String,
 });
 
-userSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
   }
   next();
@@ -22,4 +26,4 @@ userSchema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
